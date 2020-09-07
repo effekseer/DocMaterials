bfc21fbf2637a6863186bed1d9940a46cfdfccfd
durswd <swda.durl@gmail.com>
2020-09-07 15:04:12 +0900

    Update a script

--- scripts/easing.py
+++ scripts/easing.py
@@ -65,7 +65,44 @@ def EaseInBack(t):
     c = 1.8
     return (c+1) * t * t * t - c * t * t
 
+def EaseOutBack(t):
+    t = (1.0 - t)
+    c = 1.8
+    return 1.0 - ((c+1) * t * t * t - c * t * t)
 
+def EaseInOutBack(t):
+    c = 1.8
+    if t <= 0.5:
+        t *= 2.0
+        return ((c+1) * t * t * t - c * t * t) * 0.5
+    else:
+        t = (1.0 - t) * 2.0
+        return (1.0 - ((c+1) * t * t * t - c * t * t)) * 0.5
+
+def EaseOutBounce(t):
+    if t < 4.0 / 11.0:
+        t = t / 4.0 * 11.0
+        return t * t
+    elif t < 8.0 / 11.0:
+        t = t - (4.0 / 11.0) - (2.0 / 11.0)
+        return 1.0 + (t * t - (2.0 / 11.0) * (2.0 / 11.0)) * 8.0
+    elif t < 10.0 / 11.0:
+        t = t - (8.0 / 11.0) - (1.0 / 11.0)
+        return 1.0 + (t * t - (1.0 / 11.0) * (1.0 / 11.0)) * 8.0
+    else:
+        t = t - (10.0 / 11.0) - (0.5 / 11.0)
+        return 1.0 + (t * t - (0.5 / 11.0) * (0.5 / 11.0)) * 8.0
+
+def EaseInBounce(t):
+    return 1.0 - EaseOutBounce(1.0 - t)
+
+def EaseInOutBounce(t):
+    if t <= 0.5:
+        t *= 2.0
+        return EaseInBounce(t) * 0.5
+    else:
+        t = (t - 0.5) * 2.0
+        return EaseOutBounce(t) * 0.5 + 0.5
 easings = {}
 
 easings['EaseInQuadratic'] = EaseInQuadratic
@@ -85,12 +122,18 @@ easings['EaseOutQuintic'] = EaseOutQuintic
 easings['EaseInOutQuintic'] = EaseInOutQuintic
 
 easings['EaseInBack'] = EaseInBack
+easings['EaseOutBack'] = EaseOutBack
+easings['EaseInOutBack'] = EaseInOutBack
+
+easings['EaseInBounce'] = EaseInBounce
+easings['EaseOutBounce'] = EaseOutBounce
+easings['EaseInOutBounce'] = EaseInOutBounce
 
 for es_key, es_value in easings.items():
     xs = []
     ys = []
-    for t in range(100):
-        t /= 100.0
+    for t in range(200):
+        t /= 200.0
         xs.append(t)
         ys.append(es_value(t))
 
